3cda6b1049d396ab8022f585fcd617905664cc1c
Dwillwill <1529083432@qq,com>
2023-03-02 20:12:56 +0800

    Finish COW lab.

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,10 +23,16 @@ struct {
   struct run *freelist;
 } kmem;
 
+struct {
+  struct spinlock lock;
+  int refcount_arr[(PHYSTOP - KERNBASE) / PGSIZE];
+} refcount;
+
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  initlock(&refcount.lock, "refcount");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -35,8 +41,10 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+    refcount_set((uint64)p, 1);
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -50,7 +58,9 @@ kfree(void *pa)
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
-
+  if(refcount_add((uint64)pa, -1) > 0){
+    return;
+  }
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
@@ -75,8 +85,25 @@ kalloc(void)
   if(r)
     kmem.freelist = r->next;
   release(&kmem.lock);
-
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
+  if(r)
+    refcount_set((uint64)r, 1);
   return (void*)r;
+}
+
+// Set and Add the refcount
+int refcount_add(uint64 pa, int add) { 
+  int index = (pa - KERNBASE) / PGSIZE;
+  acquire(&refcount.lock);
+  refcount.refcount_arr[index] += add;
+  release(&refcount.lock);
+  return refcount.refcount_arr[index];
+}
+
+void refcount_set(uint64 pa, int ref) { 
+  int index = (pa - KERNBASE) / PGSIZE; 
+  acquire(&refcount.lock);
+  refcount.refcount_arr[index] = ref;
+  release(&refcount.lock);
 }
